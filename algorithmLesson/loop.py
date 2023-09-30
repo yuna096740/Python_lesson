3fdd1b06255f64d8c983b8665157eb658f603d7b
yuna096740 <yyuusseeii1111@gmail.com>
2023-09-30 17:36:22 +0900

    [add] loop lesson

---
+++ algorithmLesson/loop.py
@@ -0,0 +1,13 @@
+import random
+
+# リストとループ:
+# 整数のリストを受け取り、そのリスト内のすべての要素の合計を計算するPython関数を作成してください。
+
+array = [random.randint(0, 1000) for _ in range(10)] 
+
+result: int = 0 
+for i in array:
+  result += i
+
+print(array)
+print(result)
